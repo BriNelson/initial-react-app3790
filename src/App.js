bd53f4388262c23eb875c3bf60ed72492b15e188
BrNel <ladofmusic@gmail.com>
2021-12-11 03:58:20 -0700

    route worked on

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import * as React from 'react';
 
 import { ResponsiveAppBar } from "./components/NavBar";
 
-import {CongressContextProvider} from './contexts/CharactersContext'
+import { CongressContextProvider } from './contexts/CharactersContext'
+import { Route } from 'react-router-dom';
+import Welcome from './components/Welcome'
 
 function App() {
   
   return (
     <div>
-    <ResponsiveAppBar></ResponsiveAppBar>
       <CongressContextProvider>
-      <ExpenseItem></ExpenseItem>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Route path="/welcome"><Welcome /></Route>
+      
+      <Route path="/congress"><ExpenseItem/></Route>
       </CongressContextProvider>
       </div>
     
