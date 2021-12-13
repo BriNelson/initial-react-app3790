7cadb43a86bdcdb80fed8d72243dc7c3fecee53e
BrNel <ladofmusic@gmail.com>
2021-12-13 11:22:22 -0700

    readme being fixed

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
 
 
 # Harry Potter app
+
+
  
 ### 1. Effectively use conditional logic and JavaScript array methods(e.g. Filter, Map, Reduce, Find) to render large lists.
 
+<p>Can be found on line 77 of characterCardCcontainer</p>
+
 ### 2. Encapsulate your code as React functional components.
+<p>Best seen in App.js and index.js</p>
 
 ### 3. Work with command-line tools and NPM to create and manage your project within a real development toolset.
+<p>"netlify run dev" runs dev server</p>
+<p> npm dependencies used, can be seen in package.json</p>
 
 ### 4. Allow communication between components using props and the Context API. 
+<p>props passed on line 77 of CharcterCardContainer</p>
 
 ### 5. Present a form for user input that provides useful form validation and feedback for at least 3 data input items (e.g. name, password).
+<p>Signup form works and validates logout not currently working something broke. Code here, deployed example here</p>
 
-### 6. Create at least 5 custom components and use them within at least two of your other components. should be easy
+### 6. Create at least 5 custom components and use them within at least two of your other components.
+1. NavBar
+2. Login
+3. signup
+4. house cards(kinda broke, ran out of time)
+5. character cards
+6. pages (for what its worth)
+
 
 ### 7. Use a mix of CSS animations and Transition Component transitions to enhance some aspects of your project.
 
+<p>Not done<p>
+
 ### 8. Connect to a server using Netlify functions and display retrieved data including at least 5 item details (accessed by details route) in your UI. 
+<p> Netlify funcitons here <p>
 
 ### 9. Provide at least 3 different routes with navigation between them using React Router.
+<p>Routing all done with React Router V6 can be seen here on line 26 </p>
 
 ### 10. Manage your application's general and signup/login Authentication state using Hooks and the Context API. button to change
 
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,77 +2,71 @@ import React, { useState } from "react";
 
 import Card from "@mui/material/Card";
 // import Button from "@mui/material/Button";
-import CardContent from '@mui/material/CardContent';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import CardMedia from '@mui/material/CardMedia';
+import CardContent from "@mui/material/CardContent";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import CardMedia from "@mui/material/CardMedia";
 // import ImageFill from 'src/components/ravenclawBoy.png';
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
 function CharacterCard(props) {
-  
- 
-    
-  const buttonText = "favorite"
-  
+  const buttonText = "favorite";
 
   const [favorite, setFavorite] = useState(false);
 
   const clickHandler = () => {
-    
-    
     setFavorite(!favorite);
-   
-    // handleEmptyImage
-    
-  }
-// const imageReplace = () => {
-//   if (props.image != "") {
-//     return props.image
-//   } else(console.log("error")) 
-// }
 
-    return (
-        
-        <Card key={props.fullName} sx={{ minWidth: 275, m: 2}}>
-        {props.actor}
-        {(props.image !== "") ? ( <CardMedia sx={{width: "285px"}}
-        component="img"
+    // handleEmptyImage
+  };
+  // const imageReplace = () => {
+  //   if (props.image != "") {
+  //     return props.image
+  //   } else(console.log("error"))
+  // }
+
+  return (
+    <Card key={props.fullName} sx={{ minWidth: 275, m: 2 }}>
+      {props.actor}
+      {props.image !== "" ? (
+        <CardMedia
+          sx={{ width: "285px" }}
+          component="img"
           height="400"
           maxWidth="200"
-        image={props.image}
-          alt={props.fullName}/>
-        
-        ) : ( <CardMedia
+          image={props.image}
+          alt={props.fullName}
+        />
+      ) : (
+        <CardMedia
           component="img"
-            height="400"
-            width ="200"
-            image={props.replaceImage}
-            // image={ImageFill}
-            alt={props.fullName}/> )}
-              
-        
-        
-            <CardContent>
-        
+          height="400"
+          width="200"
+          image={props.replaceImage}
+          // image={ImageFill}
+          alt={props.fullName}
+        />
+      )}
+
+      <CardContent>
         <Typography variant="h5" component="div">
-        {props.fullName}
+          {props.fullName}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        {props.house}
+          {props.house}
         </Typography>
-        <Typography variant="body2">
-        {props.dateOfBirth}
-          
-          </Typography>
-          
-          <BookmarkIcon sx={{ color: favorite ? 'blue' : 'red'}}  variant="contained"  onClick={clickHandler}>{buttonText}</BookmarkIcon>
+        <Typography variant="body2">{props.dateOfBirth}</Typography>
+
+        <BookmarkIcon
+          sx={{ color: favorite ? "blue" : "red" }}
+          variant="contained"
+          onClick={clickHandler}
+        >
+          {buttonText}
+        </BookmarkIcon>
       </CardContent>
-        
-        </Card>
-            
-           
-    )
+    </Card>
+  );
 }
 
-export {CharacterCard}
+export { CharacterCard };
 // https://uvu365.sharepoint.com/sites/Course_dgm_3790_001_2021_fall_full_term-N8C6Mn7YBCfRj/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FCourse%5Fdgm%5F3790%5F001%5F2021%5Ffall%5Ffull%5Fterm%2DN8C6Mn7YBCfRj%2FShared%20Documents%2FRecorded%20Sessions%2FRecordings%2FMeeting%20in%20%5FRecorded%20Sessions%5F%2D20210922%5F143551%2DMeeting%20Recording%2Emp4&parent=%2Fsites%2FCourse%5Fdgm%5F3790%5F001%5F2021%5Ffall%5Ffull%5Fterm%2DN8C6Mn7YBCfRj%2FShared%20Documents%2FRecorded%20Sessions%2FRecordings
